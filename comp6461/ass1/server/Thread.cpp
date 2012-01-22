1d81c4ffac8054f8168fe82ab4a294d4157c4931
ewan.msn <ewan.msn@gmail.com>
2012-01-22 20:57:35 +0000

    First version. Compile OK.

--- comp6461/ass1/server/Thread.cpp
+++ comp6461/ass1/server/Thread.cpp
@@ -1,30 +1,38 @@
+/*
+ * COMP6461 Assignment1
+ *
+ * This file is modified by Yuan Tao (ewan.msn@gmail.com)
+ * Licensed under GNU GPL v3
+ *
+ * $Author: ewan.msn@gmail.com $
+ * $Date: 2011-10-12 00:12:44 -0400 (Wed, 12 Oct 2011) $
+ * $Rev: 33 $
+ * $HeadURL: https://comp6471.googlecode.com/svn/Project2/src/Project2Main.java $
+ *
+ */
+
 #include <stdio.h>
 #include "Thread.h"
 
 /*
  * This is the callback needed by the Thread class
  */
-void * Thread::pthread_callback (void * ptrThis) 
-{
+void * Thread::pthread_callback(void * ptrThis) {
 
-    if ( ptrThis == NULL )
-	     return NULL ;	
-    Thread  * ptr_this =(Thread *)(ptrThis) ;
-    ptr_this->run();
-    return NULL;
-} 
+	if (ptrThis == NULL)
+		return NULL;
+	Thread * ptr_this = (Thread *) (ptrThis);
+	ptr_this->run();
+	return NULL;
+}
 
-void Thread::start () 
-{
+void Thread::start() {
 	int result;
-    if(( result = _beginthread((void (*)(void *))Thread::pthread_callback,STKSIZE,this ))<0)
+	if ((result = _beginthread((void (*)(void *))Thread::pthread_callback,STKSIZE,this ))<0)
 	{
 		printf("_beginthread error\n");
 		exit(-1);
 	}
-    
-	
+
 }
-
-
 
--- comp6461/ass1/server/Thread.h
+++ comp6461/ass1/server/Thread.h
@@ -1,3 +1,16 @@
+/*
+ * COMP6461 Assignment1
+ *
+ * This file is modified by Yuan Tao (ewan.msn@gmail.com)
+ * Licensed under GNU GPL v3
+ *
+ * $Author: ewan.msn@gmail.com $
+ * $Date: 2011-10-12 00:12:44 -0400 (Wed, 12 Oct 2011) $
+ * $Rev: 33 $
+ * $HeadURL: https://comp6471.googlecode.com/svn/Project2/src/Project2Main.java $
+ *
+ */
+
 /*From COEN320:Real-Time System and COEN421: Embedded System Design*/
 #ifndef THREAD_HPP
 #define THREAD_HPP
@@ -7,17 +20,17 @@
 #include <process.h>
 
 #define	STKSIZE	 16536
-class Thread{
-	public:
+class Thread {
+public:
 
-		Thread()
-		{}
-		virtual ~Thread()
-		{}
+	Thread() {
+	}
+	virtual ~Thread() {
+	}
 
-		static void * pthread_callback (void * ptrThis);
+	static void * pthread_callback(void * ptrThis);
 
-		virtual void run () =0 ;
-		void  start();
+	virtual void run() =0;
+	void start();
 };
 #endif
