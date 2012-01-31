788aa3ce470786bae9417efdc90a277f66325339
ewan.msn <ewan.msn@gmail.com>
2012-01-31 01:44:14 +0000

    v0.1

---
+++ comp6421/src/LexicalAnalyzer/LexicalAnalyzerTest.java
@@ -0,0 +1,37 @@
+package LexicalAnalyzer;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class LexicalAnalyzerTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		// TODO: read testing files
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testNextToken() {
+		// TODO: compare result files with corrected files
+		
+		fail("Not yet implemented");
+	}
+
+}
